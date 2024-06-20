166d0cbc5b3887590cc89a59c7d3557db1d46a38
1Edum <eduardomatosdesouza2@gmail.com>
2024-06-20 10:44:03 -0300

    feat:add setup project

---
+++ app/src/main/java/com/example/toolbar/ListagemAluno.kt
@@ -0,0 +1,52 @@
+package com.example.toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.toolbar.databinding.ActivityListagemAlunoBinding
+
+class ListagemAluno : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityListagemAlunoBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+    //Criando o menu_principal da tela do App
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+    //Ação de clique nos itens do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Testando a ação de clique
+        when(item.itemId){
+            R.id.menu_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.menu_alunos -> {
+                Toast.makeText(this, "Alunos", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.menu_professores -> {
+                Toast.makeText(this, "Professores", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.menu_turmas -> {
+                Toast.makeText(this, "Turmas", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.menu_sair -> {
+                Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        return true
+    }
+}
